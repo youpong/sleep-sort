9fc11c41771e4dfd671c6a66a9d63c4b7d2f6cc4
NAKAJIMA Yusaku <yupong7@gmail.com>
2022-09-26 21:03:39 +0900

    wip

---
+++ go.mod
@@ -0,0 +1,3 @@
+module exmaple.com/sleep-sort
+
+go 1.19
--- sleep-sort.go
+++ sleep-sort.go
@@ -1,10 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func sleepSort(a []int) []int {
+	res := make([]int, len(a))
+	var wg sync.WaitGroup
 
-	return nil
+	tail := 0
+	var lock sync.Mutex
+	for _, v := range a {
+		wg.Add(1)
+		go func(v int) {
+			//time.Sleep(time.Duration(v) * time.Microsecond)
+			time.Sleep(time.Duration(v) * time.Millisecond)
+			lock.Lock()
+			res[tail] = v
+			tail++
+			lock.Unlock()
+			wg.Done()
+		}(v)
+	}
+	wg.Wait()
+
+	return res
 }
 
 func sortAndPrint(a []int) {
@@ -13,5 +35,8 @@ func sortAndPrint(a []int) {
 }
 
 func main() {
-
+	sortAndPrint([]int{1})
+	sortAndPrint([]int{2, 1})
+	sortAndPrint([]int{5, 4, 2, 3, 1})
+	sortAndPrint([]int{2, 1, 1, 2})
 }
